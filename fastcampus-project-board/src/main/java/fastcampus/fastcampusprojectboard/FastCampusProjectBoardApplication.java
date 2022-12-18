85de7e9af7e5f694f12ddc3f07b9c9906bf47f99
no-cy <nochanyoung0@gmail.com>
2022-12-18 22:13:08 +0900

    #6-1. 스프링 부트 프로젝트 재 시작
    사용 기술 란 :
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

--- fastcampus-project-board/src/main/java/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
+++ fastcampus-project-board/src/main/java/fastcampus/fastcampusprojectboard/FastCampusProjectBoardApplication.java
@@ -1,4 +1,4 @@
-package fastcampus.projectboard;
+package fastcampus.fastcampusprojectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
